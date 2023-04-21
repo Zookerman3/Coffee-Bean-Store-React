59629e5088e211d5e9e7849f461d0b8bbbe787fa
Zookerman3 <szook7@gmail.com>
2023-04-21 16:12:55 -0700

    intial commit

---
+++ coffee-bean-store/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+    <Header />
+    <StoreControl />
+  </React.Fragment>
+  );
+}
+
+export default App;
